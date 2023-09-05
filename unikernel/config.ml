07b6def7c29420eaeb286af4c2e7581583784aab
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-09-05 19:31:38 +0200

    Minor update with ocamlformat.0.26.0

--- unikernel/config.ml
+++ unikernel/config.ml
@@ -1,9 +1,12 @@
 open Mirage
 
-let main = foreign
-  ~packages:[ package "conan-database" ~sublibs:[ "light"; ]
-            ; package "conan" ~sublibs:[ "string" ] ]
-  "Unikernel.Make" (console @-> job)
+let main =
+  foreign
+    ~packages:
+      [
+        package "conan-database" ~sublibs:[ "light" ];
+        package "conan" ~sublibs:[ "string" ];
+      ]
+    "Unikernel.Make" (console @-> job)
 
-let () =
-  register "main" [ main $ default_console ]
+let () = register "main" [ main $ default_console ]
